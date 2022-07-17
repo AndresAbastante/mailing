0ced7c0ec1957c9fffe72f00e261a8680ae88b4c
Andres Tomas Abastante <atabastante@gmail.com>
2022-07-17 00:25:28 -0300

    asd

--- send-mail.py
+++ send-mail.py
@@ -1,1 +1,14 @@
-holi
+import keyring
+import yagmail
+
+receiver = "atabastante@gmail.com"
+body = "Hello there from Yagmail"
+filename = "document.pdf"
+
+yag = yagmail.SMTP("aaatabastante@gmail.com")
+yag.send(
+    to=receiver,
+    subject="Yagmail test with attachment",
+    contents=body, 
+    attachments=filename,
+)
